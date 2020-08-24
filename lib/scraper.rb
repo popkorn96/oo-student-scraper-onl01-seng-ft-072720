ac2a42ddbe381296f8e38962c5de276b5b05be82
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-24 14:58:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,8 @@ class Scraper
   def self.scrape_index_page(index_url)
     students = []
     html = open(index_url)
-    index  = Nokogiri::HTML
+    index  = Nokogiri::HTML(html)
+    
   end
 
   def self.scrape_profile_page(profile_url)
